e86ffffefac9492754e7cdc39449305d3613b1b8
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-04-20 22:36:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ serta pengimplementasian untuk membuat ui pada kalkulator yang ada (sederhana)
 
 '''kalkulator penjumlahan'''
 
+input_1 = st.input_number('Masukan angka 1:')
+input_2 = st.input_number('Masukan angka 2:')
+
