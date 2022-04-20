ef9ae343764638b0f088aacc84f6745955c56548
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-04-20 22:31:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,3 @@ serta pengimplementasian untuk membuat ui pada kalkulator yang ada (sederhana)
 
 '''kalkulator penjumlahan'''
 
-A = st.input(int('Insert a number:'))
-B = st.input(int('Insert a number:'))
-
-Tambah = st.button(f'Tambah {A + B}') 
-Kurang = st.button(f'Kurang {A - B}')
-Bagi = st.button(f'Bagi {A : B}')
-Kali = st.button(f'Kali {A * B}')
-
-st.write(f'Hasil tambahan:{Tambah}')
-st.write(f'Hasil tambahan:{Kurang}')
-st.write(f'Hasil tambahan:{Bagi}')
-st.write(f'Hasil tambahan:{Kali}')
