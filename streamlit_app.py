c416bfd5591cfb6c627510b85e866fcd2208af4a
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-04-20 23:10:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,15 +19,15 @@ Kali = input_1 * input_2
 Tambah, Kurang, Kali = st.columns(3)
 
 with Tambah:
-  if st.button('+', Tambah)
+  if st.button('+'):
   st.write('Hasil nya', Tambah)
 
 with Kurang:
-  if st.button('-', Kurang)
+  if st.button('-'):
   st.write('Hasil nya', Kurang)
   
 with Kali:
- if st.button('X', Kali)
+ if st.button('X'):
     st.write('Hasil nya', Kali)
 
 # st.write('Hasil nya',Num) 
