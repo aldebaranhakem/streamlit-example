a2f9a837ab9a2df75722fd8e28726913eb7668a8
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-04-21 00:33:17 +0700

    Update kalkulator_sederhana.py

--- kalkulator_sederhana.py
+++ kalkulator_sederhana.py
@@ -8,18 +8,18 @@ serta pengimplementasian untuk membuat ui pada kalkulator yang ada (sederhana)
 
 '''kalkulator penjumlahan'''
 
-input_1 = st.number_input('Masukan angka 1:', min_value=0, divide='ignore')
-input_2 = st.number_input('Masukan angka 2:', min_value=0, divide='ignore')
+input_1 = st.number_input('Masukan angka 1:', min_value=0)
+input_2 = st.number_input('Masukan angka 2:', min_value=0)
 
 Tambah = input_1 + input_2 
 Kurang = input_1 - input_2 
 Kali = input_1 * input_2
-Bagi = input_1 / input_2 
+# Bagi = input_1 / input_2 
 
 total_tambah = round(Tambah)
 total_kurang = round(Kurang)
 total_kali = round(Kali)
-total_bagi = round(Bagi)
+# total_bagi = round(Bagi)
 
 Tambah, Kurang, Kali, Bagi = st.columns([1,1,1,1])
 
@@ -35,7 +35,7 @@ with Kali:
   if st.button('X'):
      st.write('Hasil nya', total_kali)
 
-with Bagi:
-  if st.button(':'):
-     st.write('Hasil nya', total_bagi)
+# with Bagi:
+#   if st.button(':'):
+#      st.write('Hasil nya', total_bagi)
 
