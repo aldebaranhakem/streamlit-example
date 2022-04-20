ad8d49327b51d12c584cc833d24988080b1e0efd
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-04-20 23:41:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ serta pengimplementasian untuk membuat ui pada kalkulator yang ada (sederhana)
 
 '''kalkulator penjumlahan'''
 
-input_1 = st.number_input('Masukan angka 1:')
-input_2 = st.number_input('Masukan angka 2:')
+input_1 = st.number_input('Masukan angka 1:', min_value=0)
+input_2 = st.number_input('Masukan angka 2:', min_value=0)
 
 Tambah = input_1 + input_2 
 Kurang = input_1 - input_2 
@@ -20,14 +20,13 @@ Tambah, Kurang, Kali = st.columns([1,1,1])
 
 with Tambah:
   if st.button('+'):
-     st.write('Hasil nya', Tambah)
+     st.subheader('Hasil nya', Tambah)
 
 with Kurang:
   if st.button('-'):
-     st.write('Hasil nya', Kurang)
+     st.subheader('Hasil nya', Kurang)
   
 with Kali:
   if st.button('X'):
-     st.write('Hasil nya', Kali)
+     st.subheader('Hasil nya', Kali)
 
-# st.write('Hasil nya',Num) 
