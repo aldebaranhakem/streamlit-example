bdd15d38a35be76f450421ef927e6340c9ea5490
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-04-20 22:05:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
+import math as mt
 import streamlit as st
+# 
+from google.colab import drive
+drive.mount('/content/drive')
 
 """
-# Welcome to Streamlit!
+# Kalkulator sederhana
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+berikut adalah program sederhana untuk menjalankan program kalkulator menggunakan teknik python sederhana
+serta pengimplementasian untuk membuat ui pada kalkulator yang ada (sederhana)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-st.write (""" hello world """)
+'''kalkulator penjumlahan'''
 
+A = st.number_input('Insert a number:')
+B = st.number_input('Insert a number:')
 
-# with st.echo(code_location='below'):
-#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+Tambah = st.button(f'Tambah {A + B}') 
+Kurang = st.button(f'Kurang {A - B}')
+Bagi = st.button(f'Bagi {A : B}')
+Kali = st.button{f'Kali {A * B}')
 
-#     Point = namedtuple('Point', 'x y')
-#     data = []
-
-#     points_per_turn = total_points / num_turns
-
-#     for curr_point_num in range(total_points):
-#         curr_turn, i = divmod(curr_point_num, points_per_turn)
-#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-#         radius = curr_point_num / total_points
-#         x = radius * math.cos(angle)
-#         y = radius * math.sin(angle)
-#         data.append(Point(x, y))
-
-#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-#         .mark_circle(color='#0068c9', opacity=0.5)
-#         .encode(x='x:Q', y='y:Q'))
+st.write(f'Hasil tambahan:{Tambah}')
+st.write(f'Hasil tambahan:{Kurang}')
+st.write(f'Hasil tambahan:{Bagi}')
+st.write(f'Hasil tambahan:{Kali}')
