f61b71233a0db5dfef692d00e6e7fbb417954f75
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-04-20 22:48:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ serta pengimplementasian untuk membuat ui pada kalkulator yang ada (sederhana)
 input_1 = st.number_input('Masukan angka 1:')
 input_2 = st.number_input('Masukan angka 2:')
 
-Tambah = st.button('+', input_1 + input_2)  
-Kurang = st.button('-', input_1 - input_2) 
-Kali = st.button('X', input_1*input_2)
+Tambah = st.button('+', input_1 + input_2),  
+Kurang = st.button('-', input_1 - input_2), 
+Kali = st.button('X', input_1*input_2),
 
 st.write('Hasil nya',float(Tambah)) 
