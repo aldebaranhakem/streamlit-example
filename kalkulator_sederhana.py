f4619acf89505e5182ded78c0d49d112752d6b0b
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-05-12 14:32:57 +0700

    Update kalkulator_sederhana.py

--- kalkulator_sederhana.py
+++ kalkulator_sederhana.py
@@ -14,8 +14,8 @@ input_2 = st.number_input('Masukan angka 2:', min_value=0)
 Tambah = input_1 + input_2 
 Kurang = input_1 - input_2 
 Kali = input_1 * input_2
-try:
-   Bagi = input_1 / input_2 
+Bagi = input_1 / input_2 
+
 except ZeroDivisionError:
     Bagi = 0
 
