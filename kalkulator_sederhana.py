33006a1f99136fcec539b1019ba9388976305bce
Hakim <103431216+aldebaranhakem@users.noreply.github.com>
2022-05-12 14:53:29 +0700

    Update kalkulator_sederhana.py

--- kalkulator_sederhana.py
+++ kalkulator_sederhana.py
@@ -17,7 +17,7 @@ Kali = input_1 * input_2
 try:
     Bagi = input_1 / input_2    
 except ZeroDivisionError:
-    Bagi = 'null'
+    Bagi = st.write('null')
 
 total_tambah = round(Tambah)
 total_kurang = round(Kurang)
